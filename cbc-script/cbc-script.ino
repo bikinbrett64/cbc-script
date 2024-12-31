a5809b36225a0eae19be8974e79dbfe7fc808995
bikin-brett <bikinbrett64@k-state.edu>
2024-12-31 14:42:59 -0600

    Added multiple ISR's.

--- cbc-script/cbc-script.ino
+++ cbc-script/cbc-script.ino
@@ -20,12 +20,38 @@
 #define REPEAT_ACTION_DELAY_MS 200
 
 // For tracking whether the program is recording data, displaying the menu, etc.
-enum PROGRAM_STATES {
-  SIMPLE
-} programState = SIMPLE;
+enum PROGRAM_STATE {
+  SETUP,
+  MONITOR,
+  MENU
+} programState = MONITOR;
+
+// For tracking the status of a given button.
+enum BUTTON_STATE {
+  PRESS,
+  RELEASE,
+  PENDING
+};
 
 // For tracking the time elapsed since the last frame was printed.
 unsigned long frameTimer;
+
+// Variables related to the buttons.
+volatile unsigned long lastUpButtonPress = 0;
+volatile unsigned long lastDownButtonPress = 0;
+volatile unsigned long lastLeftButtonPress = 0;
+volatile unsigned long lastRightButtonPress = 0;
+volatile unsigned long lastUpButtonRelease = 0;
+volatile unsigned long lastDownButtonRelease = 0;
+volatile unsigned long lastLeftButtonRelease = 0;
+volatile unsigned long lastRightButtonRelease = 0;
+
+// For detecting an overflow of micros().
+volatile unsigned long mostRecentInput = 0;
+
+// Variables related to the magnetic sensor.
+volatile unsigned long lastMagSensorClose = 0;
+volatile unsigned long lastMagSensorOpen = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -35,7 +61,9 @@ void setup() {
 
   // TODO: Implement SD card reading/writing.
 
-  // Assigning pins for the inputs.
+  // TODO: Perform unit tests.
+
+  // Enabling the pullup resistors.
   pinMode(UP_BUTTON, INPUT_PULLUP);
   pinMode(DOWN_BUTTON, INPUT_PULLUP);
   pinMode(LEFT_BUTTON, INPUT_PULLUP);
@@ -46,5 +74,74 @@ void setup() {
 }
 
 void loop() {
-  // TODO: Everything else.
+  switch (programState) {
+    case SETUP:
+      // TODO: Ask the user for wheel diameter (and maybe other things)
+      break;
+    case MONITOR:
+      break;
+    case MENU:
+      // TODO: Implement a menu system (this will likely involve the SD card).
+      break;
+  }
+}
+
+bool microsOverflow() {
+  return micros() < mostRecentInput;
+}
+
+void queryButtons(BUTTON_STATE* up, BUTTON_STATE* down, BUTTON_STATE* left, BUTTON_STATE* right) {
+  unsigned long timeStamp = micros();
+  
+  
+}
+
+// ISR - records a press of the up button.
+void recordUpButtonPress() {
+  mostRecentInput = lastUpButtonPress = micros();
+}
+
+// ISR - records a press of the down button.
+void recordDownButtonPress() {
+  mostRecentInput = lastDownButtonPress = micros();
+}
+
+// ISR - records a press of the left button.
+void recordLeftButtonPress() {
+  mostRecentInput = lastLeftButtonPress = micros();
+}
+
+// ISR - records a press of the right button.
+void recordRightButtonPress() {
+  mostRecentInput = lastRightButtonPress = micros();
+}
+
+// ISR - records a release of the up button.
+void recordUpButtonRelease() {
+  mostRecentInput = lastUpButtonRelease = micros();
+}
+
+// ISR - records a release of the down button.
+void recordDownButtonRelease() {
+  mostRecentInput = lastDownButtonRelease = micros();
+}
+
+// ISR - records a release of the left button.
+void recordLeftButtonRelease() {
+  mostRecentInput = lastLeftButtonRelease = micros();
+}
+
+// ISR - records a release of the right button.
+void recordRightButtonRelease() {
+  mostRecentInput = lastRightButtonRelease = micros();
+}
+
+// ISR - records a closing of the magnetic sensor.
+void recordMagSensorClose() {
+  mostRecentInput = lastMagSensorClose = micros();
+}
+
+// ISR - records an opening of the magnetic sensor.
+void recordMagSensorOpen() {
+  mostRecentInput = lastMagSensorOpen = micros();
 }
