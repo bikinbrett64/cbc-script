583a67a19300ace7718d66bde29eb60b0e7f9e04
bikin-brett <bikinbrett64@k-state.edu>
2025-01-04 16:32:54 -0600

    Added buttons lol

---
+++ cbc-script/button.h
@@ -0,0 +1,78 @@
+/**
+  Just a header for handling buttons.
+
+  Author: Brett Warren
+*/
+
+#define BUTTON_DEBOUNCE_DELAY_US 5000
+#define BUTTON_HOLD_DELAY_US 1000000
+
+// For tracking the status of a given button.
+enum BUTTON_STATE {
+  PRESS,
+  RELEASE,
+  WAIT,
+  HOLD
+};
+
+// For defining a button.
+struct button {
+  int pin = -1;
+  volatile unsigned long lastPress = 0;
+  volatile unsigned long lastRelease = 0;
+  volatile bool buttonDown = false;
+  volatile bool pressHandled = true;
+};
+
+/// Returns a debounced query of the given button.
+BUTTON_STATE queryButton(button* b) {
+  if (b->buttonDown) {
+    if (!(b->pressHandled)) {
+      b->pressHandled = true;
+      return PRESS;
+    }
+    if (micros() > b->lastPress + BUTTON_HOLD_DELAY_US) {
+      return HOLD;
+    }
+    return WAIT;
+  }
+  return RELEASE;
+}
+
+/*
+class button {
+private:
+  bool buttonDown = false;
+  unsigned long lastPress = 0;
+  unsigned long lastRelease = 0;
+
+public:
+  button(int pin) {
+    pinMode(pin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(pin), recordButtonPress, FALLING);
+    attachInterrupt(digitalPinToInterrupt(pin), recordButtonRelease, RISING);
+  }
+
+  /// Returns a debounced query of the button.
+  BUTTON_STATE queryButton() {
+    unsigned long timeStamp = micros();
+    if (buttonDown) {
+      if (timeStamp - lastPress > BUTTON_DEBOUNCE_DELAY_US) {
+        return PRESS;
+      }
+      return WAIT;
+    }
+    return RELEASE;
+  }
+
+  /// ISR - updates relevant values upon a press of the button.
+  void recordButtonPress() {
+    lastPress = micros();
+  }
+
+  /// ISR - updates relevant values upon a release of the button.
+  void recordButtonRelease() {
+    lastRelease = micros();
+  }
+};
+*/
--- cbc-script/cbc-script.ino
+++ cbc-script/cbc-script.ino
@@ -4,6 +4,8 @@
   Author: Brett Warren
 */
 
+#include "button.h"
+
 // Pin definitions.
 #define UP_BUTTON 15
 #define DOWN_BUTTON 4
@@ -13,8 +15,8 @@
 
 // Timing constants.
 #define FRAME_DURATION_MS 200
-#define BUTTON_DEBOUNCE_DELAY_MS 5
-#define MAG_SENSOR_DEBOUNCE_DELAY_MS 5
+#define BUTTON_DEBOUNCE_DELAY_US 5000
+#define MAG_SENSOR_DEBOUNCE_DELAY_US 5000
 #define BUTTON_BRIEF_PRESS_MS 200
 #define BUTTON_HOLD_DELAY_MS 1000
 #define REPEAT_ACTION_DELAY_MS 200
@@ -26,25 +28,22 @@ enum PROGRAM_STATE {
   MENU
 } programState = MONITOR;
 
-// For tracking the status of a given button.
-enum BUTTON_STATE {
-  PRESS,
-  RELEASE,
-  PENDING
-};
-
 // For tracking the time elapsed since the last frame was printed.
 unsigned long frameTimer;
 
 // Variables related to the buttons.
-volatile unsigned long lastUpButtonPress = 0;
-volatile unsigned long lastDownButtonPress = 0;
-volatile unsigned long lastLeftButtonPress = 0;
-volatile unsigned long lastRightButtonPress = 0;
-volatile unsigned long lastUpButtonRelease = 0;
-volatile unsigned long lastDownButtonRelease = 0;
-volatile unsigned long lastLeftButtonRelease = 0;
-volatile unsigned long lastRightButtonRelease = 0;
+// volatile unsigned long lastUpButtonPress = 0;
+// volatile unsigned long lastDownButtonPress = 0;
+// volatile unsigned long lastLeftButtonPress = 0;
+// volatile unsigned long lastRightButtonPress = 0;
+// volatile unsigned long lastUpButtonRelease = 0;
+// volatile unsigned long lastDownButtonRelease = 0;
+// volatile unsigned long lastLeftButtonRelease = 0;
+// volatile unsigned long lastRightButtonRelease = 0;
+// volatile bool upButtonDown = false;
+// volatile bool downButtonDown = false;
+// volatile bool leftButtonDown = false;
+// volatile bool rightButtonDown = false;
 
 // For detecting an overflow of micros().
 volatile unsigned long mostRecentInput = 0;
@@ -52,31 +51,78 @@ volatile unsigned long mostRecentInput = 0;
 // Variables related to the magnetic sensor.
 volatile unsigned long lastMagSensorClose = 0;
 volatile unsigned long lastMagSensorOpen = 0;
+volatile bool magSensorClosed = false;
+
+// Instantiating the buttons.
+button up;
+button down;
+button left;
+button right;
+
+// Any additional buttons can be declared here.
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting up.");
 
-  Wire.begin();
+  up.pin = UP_BUTTON;
+  down.pin = DOWN_BUTTON;
+  left.pin = LEFT_BUTTON;
+  right.pin = RIGHT_BUTTON;
+
+  //Wire.begin();
 
   // TODO: Implement SD card reading/writing.
 
-  // TODO: Perform unit tests.
+  // TODO: Perform unit tests(?).
 
   // Enabling the pullup resistors.
-  pinMode(UP_BUTTON, INPUT_PULLUP);
-  pinMode(DOWN_BUTTON, INPUT_PULLUP);
-  pinMode(LEFT_BUTTON, INPUT_PULLUP);
-  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
+  pinMode(up.pin, INPUT_PULLUP);
+  pinMode(down.pin, INPUT_PULLUP);
+  pinMode(left.pin, INPUT_PULLUP);
+  pinMode(right.pin, INPUT_PULLUP);
   pinMode(MAG_SENSOR, INPUT_PULLUP);
+
+  // Attaching interrupts.
+  attachInterrupt(up.pin, recordUpButtonPress, FALLING);
+  attachInterrupt(down.pin, recordDownButtonPress, FALLING);
+  attachInterrupt(left.pin, recordLeftButtonPress, FALLING);
+  attachInterrupt(right.pin, recordRightButtonPress, FALLING);
+
+  attachInterrupt(up.pin, recordUpButtonRelease, RISING);
+  attachInterrupt(down.pin, recordDownButtonRelease, RISING);
+  attachInterrupt(left.pin, recordLeftButtonRelease, RISING);
+  attachInterrupt(right.pin, recordRightButtonRelease, RISING);
+
+  attachInterrupt(MAG_SENSOR, recordMagSensorClose, FALLING);  // The magnetic sensor is handled a bit differently.
+  attachInterrupt(MAG_SENSOR, recordMagSensorOpen, RISING);
 
   frameTimer = millis();
 }
 
 void loop() {
+  BUTTON_STATE upStatus, downStatus, leftStatus, rightStatus;
+  upStatus = queryButton(&up);
+  downStatus = queryButton(&down);
+  leftStatus = queryButton(&left);
+  rightStatus = queryButton(&right);
+
+  if (upStatus == PRESS) {
+    Serial.println("up");
+  }
+  if (downStatus == PRESS) {
+    Serial.println("down");
+  }
+  if (leftStatus == PRESS) {
+    Serial.println("left");
+  }
+  if (rightStatus == PRESS) {
+    Serial.println("right");
+  }
+
   switch (programState) {
     case SETUP:
-      // TODO: Ask the user for wheel diameter (and maybe other things)
+      // TODO: Ask the user for wheel diameter (and maybe other things).
       break;
     case MONITOR:
       break;
@@ -90,58 +136,69 @@ bool microsOverflow() {
   return micros() < mostRecentInput;
 }
 
-void queryButtons(BUTTON_STATE* up, BUTTON_STATE* down, BUTTON_STATE* left, BUTTON_STATE* right) {
-  unsigned long timeStamp = micros();
-  
-  
-}
-
 // ISR - records a press of the up button.
 void recordUpButtonPress() {
-  mostRecentInput = lastUpButtonPress = micros();
+  recordButtonPress(&up);
 }
 
 // ISR - records a press of the down button.
 void recordDownButtonPress() {
-  mostRecentInput = lastDownButtonPress = micros();
+  recordButtonPress(&down);
 }
 
 // ISR - records a press of the left button.
 void recordLeftButtonPress() {
-  mostRecentInput = lastLeftButtonPress = micros();
+  recordButtonPress(&left);
 }
 
 // ISR - records a press of the right button.
 void recordRightButtonPress() {
-  mostRecentInput = lastRightButtonPress = micros();
+  recordButtonPress(&right);
 }
 
 // ISR - records a release of the up button.
 void recordUpButtonRelease() {
-  mostRecentInput = lastUpButtonRelease = micros();
+  recordButtonRelease(&up);
 }
 
 // ISR - records a release of the down button.
 void recordDownButtonRelease() {
-  mostRecentInput = lastDownButtonRelease = micros();
+  recordButtonRelease(&down);
 }
 
 // ISR - records a release of the left button.
 void recordLeftButtonRelease() {
-  mostRecentInput = lastLeftButtonRelease = micros();
+  recordButtonRelease(&left);
 }
 
 // ISR - records a release of the right button.
 void recordRightButtonRelease() {
-  mostRecentInput = lastRightButtonRelease = micros();
+  recordButtonRelease(&right);
 }
 
 // ISR - records a closing of the magnetic sensor.
 void recordMagSensorClose() {
   mostRecentInput = lastMagSensorClose = micros();
+  magSensorClosed = true;
 }
 
 // ISR - records an opening of the magnetic sensor.
 void recordMagSensorOpen() {
   mostRecentInput = lastMagSensorOpen = micros();
+  magSensorClosed = false;
+}
+
+// Helper function - this is called whenever a button's pin goes low.
+void recordButtonPress(button* b) {
+  mostRecentInput = b->lastPress = micros();
+  b->buttonDown = true;
+}
+
+// Helper function - this is called whenever a button's pin goes high.
+void recordButtonRelease(button* b) {
+  mostRecentInput = b->lastRelease = micros();
+  b->buttonDown = false;
+  if (micros() > b->lastPress + BUTTON_DEBOUNCE_DELAY_US) {
+    b->pressHandled = false;
+  }
 }
